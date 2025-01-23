f7778d216afc8b82ede011b028bfe31cc644cc65
vbracken <vanessa.brckn@gmail.com>
2025-01-23 14:35:31 -0700

    Types and Variables

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert("hello world")
+//string
+
+let firstName="Mark"
+let lastName="David"
+let fullName = firstName+" "+lastName;
+console.log(fullName);
+
+//numbers
+
+let num1=10; 
+let num2=5;
+let sum1= num1+num2;
+console.log(sum1);
+
+//Boolean 
+
+let result = 50;
+console.log(result==50);
+console.log(result==30);
+
